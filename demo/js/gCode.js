833c2e3789e99a3b7f0321d2eba09e51d3745df1
og91951968 <34623686+og91951968@users.noreply.github.com>
2018-01-14 00:06:51 +0800

    更新校验码js,引入移动端mui.js插件，日历插件
    更新校验码js,引入移动端mui.js插件，日历插件

--- demo/js/gCode.js
+++ demo/js/gCode.js
@@ -1,8 +1,8 @@
 var ogcode; //在全局定义验证码  
-function createCode() {
+function createCode(id) {
 	ogcode = "";
 	var codeLength = 4; //验证码的长度  
-	var checkCode = document.getElementById("ogcode");
+	var checkCode = document.getElementById(id);
 	var random = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
 		'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'); //随机数  
 	for(var i = 0; i < codeLength; i++) { //循环操作  
@@ -12,15 +12,15 @@ function createCode() {
 	checkCode.value = ogcode; //把code值赋给验证码  
 }
 //校验验证码  
-function validate() {
-	var inputCode = document.getElementById("codevalue").value.toUpperCase(); //取得输入的验证码并转化为大写     
+function validate(id) {
+	var inputCode = document.getElementById(id).value.toUpperCase(); //取得输入的验证码并转化为大写     
 	var checkCodeFlag = false;
 	if(inputCode.length <= 0) { //若输入的验证码长度为0
 		alert("请输入验证码！"); //则弹出请输入验证码  
 	} else if(inputCode != ogcode) { //若输入的验证码与产生的验证码不一致时  
 		alert("验证码输入错误"); //则弹出验证码输入错误  
 		createCode(); //刷新验证码  
-		document.getElementById("codevalue").value = ""; //清空文本框  
+		document.getElementById(id).value = ""; //清空文本框  
 	} else { //输入正确时  
 		checkCodeFlag = true;
 	}
